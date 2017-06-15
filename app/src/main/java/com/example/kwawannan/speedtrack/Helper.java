cd25da28ad463cd73c1ec10aa4635b02ba673b18
Kwaw Annan <kwawmannanjnr@gmail.com>
2017-06-15 01:50:13 -0400

    installed ads

---
+++ app/src/main/java/com/example/kwawannan/speedtrack/Helper.java
@@ -0,0 +1,26 @@
+package com.example.kwawannan.speedtrack;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by Kwaw on 6/14/17.
+ */
+
+public class Helper {
+    public static boolean isAppRunning(final Context context, final String packageName) {
+        final ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        final List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        if (procInfos != null)
+        {
+            for (final ActivityManager.RunningAppProcessInfo processInfo : procInfos) {
+                if (processInfo.processName.equals(packageName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/kwawannan/speedtrack/MainActivity.java
+++ app/src/main/java/com/example/kwawannan/speedtrack/MainActivity.java
@@ -17,13 +17,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.anastr.speedviewlib.AwesomeSpeedometer;
-
-import hotchemi.android.rate.AppRate;
-import hotchemi.android.rate.OnClickButtonListener;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     TextView textView;
     AwesomeSpeedometer awesomeSpeedometer;
+    private AdView mAdView;
+    InterstitialAd mInterstitialAd;
+
+
     private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
 
     @Override
@@ -33,23 +38,25 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppRate.with(this)
-                .setInstallDays(0) // default 10, 0 means install day.
-                .setLaunchTimes(2) // default 10
-                .setRemindInterval(2) // default 1
-                .setShowLaterButton(true) // default true
-                .setDebug(true) // default false
-                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
-                    @Override
-                    public void onClickButton(int which) {
-                        Log.d(MainActivity.class.getName(), Integer.toString(which));
-                        launchMarket();
-                    }
-                })
-                .monitor();
-
-        // Show a dialog if meets conditions
-        AppRate.showRateDialogIfMeetsConditions(this);
+//        AppRate.with(this)
+//                .setInstallDays(0) // default 10, 0 means install day.
+//                .setLaunchTimes(2) // default 10
+//                .setRemindInterval(2) // default 1
+//                .setShowLaterButton(true) // default true
+//                .setDebug(true) // default false
+//                .setOnClickButtonListener(new OnClickButtonListener() { // callback listener.
+//                    @Override
+//                    public void onClickButton(int which) {
+////                        Log.d(MainActivity.class.getName(), Integer.toString(which));
+////                        launchMarket();
+//                        Toast.makeText(this," went wrong", Toast.LENGTH_LONG).show();
+//
+//                    }
+//                })
+//                .monitor();
+//
+//        // Show a dialog if meets conditions
+//        AppRate.showRateDialogIfMeetsConditions(this);
         awesomeSpeedometer = (AwesomeSpeedometer) findViewById(R.id.speedView);
         awesomeSpeedometer.setUnit("MPH");
         awesomeSpeedometer.setSpeedTextSize(110);
@@ -59,8 +66,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+
+        }else{
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
                     MY_PERMISSION_ACCESS_COARSE_LOCATION);
         }
@@ -69,6 +78,41 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         this.onLocationChanged(null);
+        Toast.makeText(this, "Please drive around to see change in speed", Toast.LENGTH_LONG).show();
+
+        //Ads
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+
+        mInterstitialAd = new InterstitialAd(this);
+
+        // set the ad unit ID
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_full_screen));
+
+        AdRequest adRequest2 = new AdRequest.Builder()
+                .build();
+
+        // Load ads into Interstitial Ads
+        mInterstitialAd.loadAd(adRequest2);
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            public void onAdLoaded() {
+                showInterstitial();
+            }
+        });
+
+
+        if (Helper.isAppRunning(MainActivity.this, "com.your.desired.app")) {
+            // App is running
+        } else {
+            // App is not running
+        }
+
+
+
+
     }
 
     @Override
@@ -128,6 +172,33 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show();
         }
     }
+    private void showInterstitial() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        }
+    }
 
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
 }
